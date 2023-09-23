afc35bfa81d0f2bb8f0e42c44c56ed4ee6131c59
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-23 17:28:43 +0300

    feat: explore guardian api

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
-import Link from 'next/link';
 import React from 'react';
+import getConfig from 'next/config'
+import { ArticleCard } from '@/components/article-card';
 
-export default function Home() {
+
+export default async function Home() {
+	const { publicRuntimeConfig } = getConfig();
+
+	const apiKey = publicRuntimeConfig.GUARDIAN_API_KEY;
+	const apiUrl = `${publicRuntimeConfig.GUARDIAN_API_URL}search?show-fields=body,headline,thumbnail&api-key=${apiKey}`;
+
+	const res = await fetch(apiUrl, { cache: 'no-store' });
+	const data: any = await res.json();
+	console.log('data', data);
+
+	const news = await data.response.results;
+	console.log('news', news);
+
 	return (
 		<main>
 			Home page
 			<div>
-				<Link href={ '/article' }>ArticlePage</Link>
+				{news.map((item: any) => (
+					<ArticleCard key={item.id} item={item} />
+				))}
 			</div>
 		</main>
-	)
+	);
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  publicRuntimeConfig: {
+    GUARDIAN_API_KEY: process.env.GUARDIAN_API_KEY,
+    GUARDIAN_API_URL: process.env.GUARDIAN_API_URL
+  },
+  images: {
+    unoptimized: true,
+    domains: [
+      'media.guim.co.uk',
+    ]
+
+  },
+}
 
 module.exports = nextConfig

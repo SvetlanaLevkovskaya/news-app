131c7f68c28079aab4f448800bc7a1354e93809d
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-23 14:58:18 +0300

    set up project

---
+++ app/article/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ArticlePage = () => {
+	return (
+		<div>
+			ArticlePage
+		</div>
+	);
+};
+
+export default ArticlePage
+
---
+++ redux/features/news-slice.ts
@@ -0,0 +1,15 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type initialState = {}
+
+const initialState = {}
+
+export const news = createSlice({
+	name: 'news',
+	initialState,
+	reducers: {},
+})
+
+export const {  } = news.actions
+export default news.reducer
+
